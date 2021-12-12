50889cfb295f53c9402f891fb4a62760f2e400d4
Girish-del <72304710+Girish-del@users.noreply.github.com>
2021-12-12 23:31:10 +0530

    project complete

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2021 PES MCOE</p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
